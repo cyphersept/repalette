0a847b15f1e685e7430e9b2065404ec413fa57a6
cypher <cyphersept@protonmail.com>
2024-05-09 23:19:51 -0500

    WIP debug displays

--- repalette.js
+++ repalette.js
@@ -3,8 +3,9 @@
 
 const displayCanvas = document.getElementById("output-canvas");
 const ctx = displayCanvas.getContext("2d");
+const displayOriginal = document.querySelector("img.display")
 
-const hiddenCanvas = document.getElementById("hidden-canvas");
+const hiddenCanvas = document.createElement("canvas");
 const htx = hiddenCanvas.getContext("2d");
 const images = {};
 const palettes = {};
@@ -24,71 +25,54 @@ document.querySelector(".repalette").onclick = function() {repalette};
 // ============================
 
 // Process user-uploaded palette from file or URL
-function uploadFiles(input, type) {
-    const arr = (type == "image") ? images : palettes;
+async function uploadFiles(input, type) {
+    const arr = (type == "image")? images : palettes;
+    let last;
 
+    // Upload/update image only if it has changed
     for (const file of input.files) {
-        if (arr[file.name] && arr[file.name].date == file.lastModified) {
-            return false;
+        if (type == "image") {
+            if (arr[file.name] && arr[file.name].date == file.lastModified) return false;
+            last = await addImage(file);
+            console.log(last.obj);
         }
-        else {
-            if (type == "image") console.log(addImage(file));
-            else if (type == "palette") console.log(addPalette(file));
+        else if (type == "palette") {
+            last = await addPalette(file);
+            console.log(last.obj);
         }
     }
+    // Select the last-created node
+    switchSelected(last.node);
 }
 
 
-// function uploadPalette(input) {
-//     const img = new Image();
-//     const error = document.querySelector(".palettes .error")
-
-//     // If the url is not a valid image, display error
-//     img.onerror = () => { 
-//         console.log("Invalid URL")
-//         //error.hidden = false;
-//     }
-//     // If the image loads, add the palette list to the DOM
-//     img.onload = () => {
-//         console.log("Successfully loaded: " + img.width)
-//         if (img.width > 0) {
-//             const palette = paletteFromImg(loadImageToCanvas(img, hiddenCanvas, htx))
-//             error.hidden = true;
-//             document.querySelector(".palettes.list").appendChild(addPalette(palette))
-//         }
-//     }
-//     // Set image source if input is a file
-//     if (input instanceof File) {
-//         const reader = new FileReader();
-//         reader.onload = function(e) { img.src = e.target.result };
-//         reader.readAsDataURL(input);
-//     } 
-//     // Set image source if input is a URL
-//     else if (typeof input.value === "string") {
-//         img.src = input.value.trim();
-//         //document.querySelector(".palettes.list").appendChild(img)
-//     }
-// }
-
 // Create DOM display of palette information-- returns node 
 function addPalette(file) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-        const img = new Image();
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        let palette;
 
-        // Update and process image data once file is loaded
-        img.onload = () => {
-            console.log("Successfully loaded: " + img.width)
-            if (img.width > 0) {
-                const palette = paletteFromImg(loadImageToCanvas(img, hiddenCanvas, htx))
-                createPaletteNode(palette);
-            }
+         // Update and process image data once reader and image load
+        reader.onload = (e) => {
+            const img = new Image();
+            img.onload = () => {
+                if (img.width > 0) {
+                    palette = paletteFromImg(loadImageToCanvas(img, hiddenCanvas, htx));
+                    palette.node = createPaletteNode(palette);
+                    resolve(palette); // Resolve the promise with the palette object
+                } else {
+                    reject(new Error("Image width is 0"));
+                }
+            };
+            img.src = e.target.result;
         };
 
-        img.src = e.target.result;
-    }
-    reader.readAsDataURL(file);
-    return palettes[file.name];
+        // Reject the promise on file read error
+        reader.onerror = (error) => {reject(error)};
+        
+        // Attempt to read file
+        reader.readAsDataURL(file);
+    });
 }
 
 function createPaletteNode(obj) {
@@ -178,38 +162,43 @@ function imgFromUrl(id) {
 
 // Create imgObj of image information
 function addImage(file) {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-        // Create Image
-        const img = new Image();
-        const name = file.name;
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            // Create Image
+            const img = new Image();
+            const name = file.name;
 
-        // Update and process image data once file is loaded
-        img.onload = () => {
-            // If the file is new, append an element to the DOM
-            if (!images[name]) {
-                createImgNode(file)
-            }
-
-            // Update the file's associated obj data
-            images[name].name = name
-            images[name].date = file.lastModified;
-            images[name].img = img;
-            images[name].imgData = loadImageToCanvas(img, hiddenCanvas, htx);
-            images[name].defaultPalette = paletteFromImg(images[name].imgData);
-            images[name].repalettes = {}; // Stores imgData of processed alt palettes
+            // Update and process image data once file is loaded
+            img.onload = () => {
+                // If the file is new, append an element to the DOM
+                if (!images[name]) {
+                    images[name] = {node: createImgNode(file)};
+                }
+                
+                // Update the file's associated obj data
+                images[name].name = name;
+                images[name].date = file.lastModified;
+                images[name].img = img;
+                images[name].imgData = loadImageToCanvas(img, hiddenCanvas, htx);
+                images[name].defaultPalette = paletteFromImg(images[name].imgData);
+                images[name].repalettes = {}; // Stores imgData of processed alt palettes
+                
+                resolve(images[name]); // Resolve the promise with the updated images object
+            };
+            img.src = e.target.result; 
         };
-        
-        // Trigger image loading and reader
-        img.src = e.target.result; 
-    };
-    reader.readAsDataURL(file);
-    return images[file.name]
+
+        // Reject the promise if there's an error reading the file
+        reader.onerror = (error) => {
+            reject(error); 
+        };
+        reader.readAsDataURL(file);
+    });
 }
 
 function createImgNode(file) {
     const el = imgTemplate.cloneNode(true)
-    images[file.name] = {};
     el.lastElementChild.textContent = file.name;
     el.dataset.obj = images[file.name];
     el.classList.remove("template");
@@ -227,23 +216,39 @@ function deleteImage(el) {
 // #endregion
 
 
-// Convert image to canvas API data
-function loadImageToCanvas(img, canvas, context) {
+// Also converts image to canvas API data
+function loadImageToCanvas(img, canvas, context, returnData = true) {
     canvas.width = img.naturalWidth;
     canvas.height = img.naturalHeight;
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(img, 0, 0, canvas.width, canvas.height);
-    let imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-    console.log(imgData);
-
-    return imgData;
+    
+    if (returnData) {
+        let imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+        console.log(imgData);
+        return imgData;
+    }
 }
 
 //Toggles selected element in same parent group
 function switchSelected(next) {
     const prev = next.parentElement.querySelector(".selected");
-    prev.classList.toggle("selected");
+
+    // Highlights selected object in list
+    if (prev) prev.classList.toggle("selected");
     next.classList.toggle("selected");
+
+    // Displays selected image
+    if (next.classList.contains("image")) {
+        const obj = next.obj;
+        // Changes left display
+        displayOriginal.src = obj.img.src;
+        displayOriginal.width = obj.img.width;
+        displayOriginal.height = obj.img.height;
+        // Changes right display according to selected palette
+        loadImageToCanvas(obj.img, displayCanvas, ctx, false);
+        
+    }
 }
 
 
@@ -378,3 +383,13 @@ function repalette(imgObj, newPalette, show=true) {
 
 // https://github.com/leeoniya/RgbQuant.js <- For simplifying 
 
+//region To-Do
+/*
+    - debug repalette
+    - add function for bulk process
+    - implement drag and drop
+    - display palettes by name, expand when focused
+    - allow color deletion
+    - (stretch): color picker
+    - (bloat): change amount of allowed colours
+*/
